f26f4308f2a9ad34870120efd0cb5ddbd95a8b77
MaksimSimpleCode <80915302+MaksimSimpleCode@users.noreply.github.com>
2022-11-19 00:10:19 +0400

    Добавьте файлы проекта.

---
+++ File System Manager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace File_System_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://learn.microsoft.com/ru-ru/dotnet/csharp/programming-guide/concepts/linq/how-to-query-for-duplicate-files-in-a-directory-tree-linq
+            /*https://learn.microsoft.com/ru-ru/troubleshoot/developer/visualstudio/csharp/language-compilers/create-file-compare
+             * https://www.cyberforum.ru/csharp-beginners/thread1428441.html
+             * https://ru.stackoverflow.com/questions/743761/%D0%A1%D1%80%D0%B0%D0%B2%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B1%D0%BE%D0%BB%D1%8C%D1%88%D0%B8%D1%85-%D0%BD%D0%B5%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D1%85-%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2
+             */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
